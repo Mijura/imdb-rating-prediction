f8896d105b18157598b9ee00b2d84995371721c4
C0mpy <n.zunic@yahoo.com>
2017-06-24 14:04:06 +0200

    minor code refactoring

--- app.py
+++ app.py
@@ -3,37 +3,55 @@ from pandas import DataFrame,Series
 import numpy as np
 from sklearn import linear_model
 
-data = DataFrame(pd.read_csv('movie_metadata.csv'))
-data = data[data.dtypes[data.dtypes!='object'].index]
-data = data.fillna(0)
-df_norm = (data - data.mean()) / (data.max() - data.min()) + 1
+def ridge_regression(x_train, y_train, x_test):
+	model = linear_model.Ridge()
+	model.fit(x_train, y_train)
+
+	return model.predict(x_test)
+	
+def calc_error(y_test, y_predict):
+
+	err = 0
+	s = 0
+
+	for y, yp in zip(y_test, y_predict) :
+		print(y, yp)
+		s += abs(y - yp)
+		err += (y - yp) ** 2
+
+	print(err, len(x_test))
+	print(err / len(x_test))
+	print(s / len(x_test))
+
+if __name__ == "__main__":
+	data = DataFrame(pd.read_csv('movie_metadata.csv'))
+	data = data[data.dtypes[data.dtypes!='object'].index]
+	data = data.fillna(0)
+	df_norm = (data - data.mean()) / (data.max() - data.min()) + 1
 
 
-x_train = data[data.dtypes[data.dtypes != 'object'].index]
-y_train = data['imdb_score']
-x_train.drop(['imdb_score'],axis = 1, inplace = True)
-x_train = x_train.fillna(0)
+	x_train = data[data.dtypes[data.dtypes != 'object'].index]
+	y_train = data['imdb_score']
+	x_train.drop(['imdb_score'],axis = 1, inplace = True)
+	x_train = x_train.fillna(0)
 
-x_test = data[2000:]
-y_test = x_test['imdb_score']
-x_test.drop(['imdb_score'], axis = 1, inplace = True)
+	x_test = data[2000:]
+	y_test = x_test['imdb_score']
+	x_test = x_test.drop(['imdb_score'], axis = 1)
 
-x_train = x_train[:2000]
-y_train = y_train[:2000]
+	x_train = x_train[:2000]
+	y_train = y_train[:2000]
+	
+	choice = -1
+	while True:
+		choice = int(input("\nChoose an algorithm: \n1.Ridge Regression\n2.\n3.\n0. exit\n"))
 
-model = linear_model.Ridge()
-model.fit(x_train, y_train)
-
-y_predict = model.predict(x_test)
-
-err = 0
-s = 0
-
-for y, yp in zip(y_test, y_predict) :
-	print(y, yp)
-	s += abs(y - yp)
-	err += (y - yp) ** 2
-
-print(err, len(x_test))
-print(err / len(x_test))
-print(s / len(x_test))
+		if choice == 1:
+			y_predict = ridge_regression(x_train, y_train, x_test)
+			calc_error(y_test, y_predict)
+		elif choice == 2:
+			pass
+		elif choice == 3:
+			pass
+		elif choice == 0:
+			break
